403507cead810e9c6f5fc26558fb4ce429d6ba2c
AliNemat <nematba@ucr.edu>
2020-05-27 17:00:22 -0400

    meme_xml_parser works well

--- meme_xml_parser.py
+++ meme_xml_parser.py
@@ -5,35 +5,35 @@ import shutil
 import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input_bed_file' , dest='input_bed_file', required=True, help='name of bed input file with absolute directory address ')
-parser.add_argument('--input_xml_file' , dest='input_xml_file', required=True, help='name of xml input file with absolute directory address ')
-parser.add_argument('--output_bed_path' , dest='output_bed_path', required=True, help='desired path of writing bed output file with absolute directory address')
-parser.add_argument('--output_xml_path' , dest='output_xml_path', required=True, help='desired path of writing xml output file with absolute directory address')
+parser.add_argument('--input_xml_file' , dest='input_xml_files', action='append', required=True, help='Galaxy name with absolute directory address of xml input file')
+parser.add_argument('--input_file_name' , dest='input_files_name', action='append', required=True, help='Real name of xml input file')
+parser.add_argument('--output_xml_path' , dest='output_xml_path', required=True, help='Desired path of writing xml output file with absolute directory address')
 args = parser.parse_args()
-time.sleep(60)
+#time.sleep(60)
 
 
-tree = ET.parse(args.input_xml_file)
-root = tree.getroot()
 
-for item in root.findall("motifs"):
-    result = len(list(item))
+txt_file_name = "subtype_meme_motif_info.txt"
+fileM = open(txt_file_name,'w')
+fileM.write ("subtype, meme_motif_found\n")
 
-bed_filename_tmp = ( ((args.input_bed_file).split('/'))[-1])
-bed_filename     = bed_filename_tmp.split('.')[0]
-xml_filename_tmp = ( ((args.input_xml_file).split('/'))[-1])
-xml_filename     = xml_filename_tmp.split('.')[0]
-if (args.output_bed_path[-1] != '/'):
-    args.output_bed_path = args.output_bed_path + '/'
 
 if (args.output_xml_path[-1] != '/'):
     args.output_xml_path = args.output_xml_path + '/'
 
-if (result == 0):
-    shutil.copy (args.input_bed_file, args.output_bed_path + bed_filename + '.bed')
-    shutil.copy (args.input_xml_file, args.output_xml_path + xml_filename + '.xml')
-else:
-    shutil.copy (args.input_bed_file, args.output_bed_path + bed_filename  + '.bed')
-    shutil.copy (args.input_xml_file, args.output_xml_path + xml_filename  + '.xml')
+
+for (input_xml_file, file_name) in zip (args.input_xml_files, args.input_files_name):
+    
+    tree = ET.parse(input_xml_file)
+    root = tree.getroot()
+    
+    for item in root.findall("motifs"):
+        result = len(list(item))
+    
+    if (result == 0):
+        fileM.write (str(file_name) + ", " + str(result) + '\n')
+    else:
+        shutil.copy (input_xml_file, args.output_xml_path + file_name  + '.xml')
+        fileM.write (str(file_name) + ", " + str(result) + ' \n')
 
 #time.sleep(180)
